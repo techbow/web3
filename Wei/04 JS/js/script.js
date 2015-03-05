7e16d80b653a5700ab8c11ec4e83c59a13541c0a
Victor <hanweihw@gmail.com>
2015-03-04 17:21:28 -0800

    04 note

--- Wei/04 JS/js/script.js
+++ Wei/04 JS/js/script.js
@@ -1,12 +1,31 @@
-var textClosure = function() {
-	var x = 4;
-	var closeX = function() {
-		return x;
+function assignTorpedo ( name, passengerArray ){
+	var torpedoAssignment;
+
+
+	for (var i = 0; i<passengerArray.length; i++) {
+		if (passengerArray[i] == name) {
+			torpedoAssignment = function ( ) {
+				var j = i; //does not work
+				// alert("Ahoy, " + name + "!\n" + "Man your post at Torpedo #" + (j+1) + "!");
+
+			};
+		}
 	}
-	return closeX;
+	return torpedoAssignment;
 }
 
-// try first
+var subPassengers = ["Luke", "Leia", "Han", "Chewie", "Yoda", "R2-D2", "C-3PO", "Boba"];
+var giveAssignment = assignTorpedo("Chewie", subPassengers);
 
-textClosure();
-textClosure()();
+giveAssignment();
+
+
+var myBox = {
+	height: 6,
+	width: 8,
+	length: 10,
+	volume: 720,
+	material: "cardboard",
+};
+
+Object.prototype.color = "red";
